af67a9558169932ea677f0eae282af77f72ff007
dandirizardi <dandirizardi36@gmail.com>
2023-03-28 21:18:05 +0700

    Upload file Latihan  & Tugas3

---
+++ file_array.php
@@ -0,0 +1,13 @@
+<?php 
+//membuat array scalar(satu dimensi)
+$ar_buah = ['pepaya','mangga','pisang','jambu'];
+
+foreach($ar_buah as $id => $buah){
+    echo '<br> Tampilkan key'.$id;
+}
+echo'<hr>';
+foreach($ar_buah as $buah){
+    echo '<br> Tampilkan data '.$buah;
+}
+
+?>
